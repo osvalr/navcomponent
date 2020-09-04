37319a7f30fb35f9a7cb423f146f899a7ca5e679
Osval Reyes <osvaljr2@gmail.com>
2020-09-04 17:48:23 -0300

    add project and initial fragments

---
+++ app/src/main/java/com/osvalr/navigationexample/ui/MainActivity.java
@@ -0,0 +1,16 @@
+package com.osvalr.navigationexample.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.osvalr.navigationexample.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
